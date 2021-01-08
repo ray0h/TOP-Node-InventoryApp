7e10f50be88c4b1dd480e53a48a6655af7599ac9
Raymond Oh <raymond.oh@gmail.com>
2021-01-08 13:54:19 -0500

    created new grocery form and create controller functions

--- controllers/groceryController.js
+++ controllers/groceryController.js
@@ -1,4 +1,6 @@
 const Grocery = require('../models/grocery');
+const Category = require('../models/category');
+const Location = require('../models/location');
 
 const { body, validationResult } = require('express-validator');
 const async = require('async');
@@ -8,13 +10,65 @@ exports.index = (req, res) => {
 };
 
 // Create a grocery
-exports.grocery_create_get = (req, res) => {
-  res.send('Not implemented yet, grocery_create_get');
+exports.grocery_create_get = (req, res, next) => {
+  async.parallel({
+    categories: function(callback) { Category.find(callback) },
+    locations: function(callback) { Location.find(callback) },
+  }, function(err, results) {
+    if (err) { return next(err); }
+    res.render('grocery_form', { title: 'Create a new Grocery', categories: results.categories, locations: results.locations });
+  });
 };
 
-exports.grocery_create_post = (req, res) => {
-  res.send('Not implemented yet, grocery_create_post');
-};
+exports.grocery_create_post = [
+  // Validate and sanitize fields
+  body('name', 'Name must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('description', 'Description must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('price', 'Price must not be empty.').escape(),
+  body('inventory', 'Inventory must not be empty.').escape(),
+  body('category', 'Category must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('location', 'Location must not be empty.').trim().isLength({ min: 1 }).escape(),
+  // plu is optional
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a new Grocery object with escaped and trimmed data.
+    let grocery = new Grocery(
+      {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        inventory: req.body.inventory,
+        category: req.body.category,
+        location: req.body.location,
+        plu: req.body.plu 
+      }
+    );
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      async.parallel({
+        categories: function(callback) { Category.find(callback) },
+        locations: function(callback) { Location.find(callback) },
+      }, function(err, results) {
+        if (err) { return next(err); }
+
+        res.render('grocery_form', { title: 'Create a new Grocery', categories: results.categories, locations: results.locations, errors: errors.array() });
+      });
+      return;
+    } else {
+      // Data from form is valid.  Save grocery
+      grocery.save(function(err) {
+        if (err) { return next(err); }
+        // successful - redirect to new grocery
+        res.redirect(grocery.url);
+      });
+    }
+  }
+];
 
 // Display list of all groceries
 exports.grocery_list = (req, res) => { 
--- models/grocery.js
+++ models/grocery.js
@@ -5,7 +5,7 @@ const GrocerySchema = new Schema(
     name: { type: String, required: true, maxlength: 100 },
     description: { type: String, required: true },
     price: { type: mongoose.Decimal128, required: true },
-    inventory: { type: Number, default: 0 },
+    inventory: { type: Number, default: 0, required: true },
     plu: String,
     category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
     location: { type: Schema.Types.ObjectId, ref: 'Location', required: true }
